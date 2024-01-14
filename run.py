1c0d39e96e8968d60e9468f96d41276919af4ff2
FFaanes <146946191+FFaanes@users.noreply.github.com>
2024-01-14 16:05:00 +0100

    Updated google_search_count result header

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class Bedrift(Resource):
    def get(self, nr_or_name):
         if "api-key" in request.headers and request.headers["api-key"] in api_keys:
-            return search_company(nr_or_name, validate_emails=request.headers["validate-emails"], similar_results=5)
+            return search_company(nr_or_name, validate_emails=request.headers["validate-emails"], similar_results=5, google_search_count=int(request.headers["google-search-count"]))
         else:
             return {"error":"access restricted"}
 
