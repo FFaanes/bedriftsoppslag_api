3dbc2233bb2cfb5903889fffaab7f6f972b18cf2
FFaanes <146946191+FFaanes@users.noreply.github.com>
2024-01-24 11:48:05 +0100

    Added search history management

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import urllib3
 import pickle
 import os
+import datetime
 
 from flask import Flask, request
 from flask_restful import Api, Resource
@@ -44,12 +45,42 @@ class CacheManager:
 
   
 
+class SearchHistoryManager:
+    def __init__(self):
+        try:
+            self.history = self.load_history_pickle()
+        except FileNotFoundError:
+            with open(f"{base_path}/history.dat", "wb") as historyfile:
+                self.history = ["history", {}]
+                pickle.dump(self.history, historyfile)
+    
+    def clear_history(self):
+        with open(f"{base_path}/history.dat", "wb") as historyfile:
+            self.history = ["history", {}]
+            pickle.dump(self.history, historyfile)
+
+
+    def save_history(self):
+        with open(f"{base_path}/history.dat", "wb") as historyfile:
+            pickle.dump(self.history, historyfile)
+
+    
+    def load_history_pickle(self):
+        with open(f"{base_path}/history.dat", "rb") as historyfile:
+            return pickle.load(historyfile)
+
 
 
 # -------------------------------- Get company info  ------------------------------------------------------#
 class Bedrift(Resource):
    def get(self, nr_or_name):
         if "api-key" in request.headers and request.headers["api-key"] in api_keys:
+
+            # Save History for statistics on admin page
+            current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            searchhistorymanager.history[1][str(current_time)] = {"user": request.headers["current-user"], "search" : nr_or_name}
+            searchhistorymanager.save_history()
+
             # Return cached data if it exists.
             if nr_or_name in cachemanager.cache[1]:
                 return cachemanager.cache[1][nr_or_name]
@@ -86,15 +117,34 @@ class ClearCache(Resource):
             return {"error":"access restricted"}
 
 
+# -------------------------------- Retrieve Search History  ------------------------------------------------------#
+class SearchHistory(Resource):
+    def get(self):
+        if "api-key" in request.headers and request.headers["api-key"] in api_keys:
+
+            if "mode" in request.headers and request.headers["mode"] == "load":
+                return searchhistorymanager.load_history_pickle()
+            else:
+                searchhistorymanager.clear_history()
+                return "history deleted!"
+
+        else:
+            return {"error":"access restricted"}
+
+
+
+
+
 
 # -------------------------------- Prepare and run app  ------------------------------------------------------#
 
 api.add_resource(Bedrift, "/bedrift/<nr_or_name>")
 api.add_resource(OppdaterData, "/api/oppdaterdata")
 api.add_resource(ClearCache,"/api/clearcache")
+api.add_resource(SearchHistory,"/api/searchhistory")
 
 if __name__ == "__main__":
-    # Create Cache Manager object
+    searchhistorymanager = SearchHistoryManager()
     cachemanager = CacheManager()
 
     app.run(debug=True, host="127.0.0.1", port=5001)
